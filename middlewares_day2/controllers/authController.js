ca66f084e009d40e8f4a79348fe9567190ac4cb9
eppalapallysharath <eppalapallysharath@gmail.com>
2025-07-14 17:03:43 +0530

    router middleware and controllers

---
+++ middlewares_day2/controllers/authController.js
@@ -0,0 +1,13 @@
+function loginController(req, res) {
+  const userData = { email: "vineeth@gmail.com", password: "@1234" };
+
+  const { email, password } = req.body;
+
+  const profile =
+    email === userData.email && password === userData.password
+      ? "login success"
+      : "login failed";
+  return res.send(profile);
+}
+
+module.exports = { loginController };
---
+++ middlewares_day2/controllers/productsController.js
@@ -0,0 +1,5 @@
+const getProducts = (req, res) => {
+  return res.send("all products");
+};
+
+module.exports = { getProducts };
